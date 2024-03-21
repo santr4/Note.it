468b8b2cc2213b796adb47bf697bb958dcb2b41a
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2024-03-22 00:03:51 +0530

    [add] : todolist connected with firestore

--- note.it/src/app/(to-do)/_components/todolist.tsx
+++ note.it/src/app/(to-do)/_components/todolist.tsx
@@ -1,25 +1,54 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
+import { db } from "@/app/config/firebase";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  deleteDoc,
+  getDocs,
+  doc,
+} from "firebase/firestore";
 
 const Todo = () => {
   const [task, setTask] = useState("");
   const [tasksArray, setTasksArray] = useState<string[]>([]);
 
-  const inputChange = (e: any) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const todoCollection = collection(db, "todos");
+      const todoSnapshot = await onSnapshot(
+        query(todoCollection),
+        (snapshot) => {
+          const tasksData = snapshot.docs.map((doc) => doc.data().task);
+          setTasksArray(tasksData);
+        }
+      );
+    };
+    fetchData();
+  }, []);
+
+  const inputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTask(e.target.value);
   };
 
-  const inputSubmit = (e: any) => {
+  const inputSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (task.trim()) {
+      await addDoc(collection(db, "todos"), { task });
       setTasksArray([...tasksArray, task]);
       setTask("");
     }
   };
 
-  const handleDelete = (index: any) => {
+  const handleDelete = async (index: any) => {
+    const todoCollection = collection(db, "todos");
+    const snapshot = await getDocs(todoCollection);
+    const docId = snapshot.docs[index].id;
+    await deleteDoc(doc(todoCollection, docId));
     setTasksArray(tasksArray.filter((_, i) => i !== index));
   };
 
