3208f4ba6762d09303c6fbdf4a9b487cf05ae5b1
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2024-04-19 23:15:19 +0530

    [added] : post request created for reading list

--- note.it/src/app/(readinglist)/_components/readinglist.tsx
+++ note.it/src/app/(readinglist)/_components/readinglist.tsx
@@ -1,16 +1,17 @@
 "use client";
+import axios from "axios";
 
 import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
-import {
-  collection,
-  addDoc,
-  getDocs,
-  doc,
-  getDoc,
-  deleteDoc,
-} from "firebase/firestore";
-import { db } from "@/app/config/firebase";
+// import {
+//   collection,
+//   addDoc,
+//   getDocs,
+//   doc,
+//   getDoc,
+//   deleteDoc,
+// } from "firebase/firestore";
+// import { db } from "@/app/config/firebase";
 
 interface Book {
   title: string;
@@ -27,50 +28,77 @@ const ReadingList = () => {
   }, []);
 
   const fetchBooks = async () => {
-    const booksCollection = collection(db, "books");
-    const querySnapshot = await getDocs(booksCollection);
-    const fetchBooks: Book[] = [];
-    querySnapshot.forEach((doc) => {
-      const bookData = doc.data();
-      fetchBooks.push({
-        title: bookData.title,
-        author: bookData.author,
-      });
-    });
-    setBooks(fetchBooks);
+    // const booksCollection = collection(db, "books");
+    // const querySnapshot = await getDocs(booksCollection);
+    // const fetchBooks: Book[] = [];
+    // querySnapshot.forEach((doc) => {
+    //   const bookData = doc.data();
+    //   fetchBooks.push({
+    //     title: bookData.title,
+    //     author: bookData.author,
+    //   });
+    // });
+    // setBooks(fetchBooks);
+    try {
+      const response = await axios.get(
+        "http://localhost:8000/api/get_readinglist/"
+      );
+      const fetchedBooks: Book[] = response.data.data.map((book: any) => ({
+        task_id: book.task_id,
+        title: book.title,
+        author: book.author,
+      }));
+      setBooks(fetchedBooks);
+    } catch (error) {
+      console.error("error fetching data: ", error);
+    }
   };
 
   const addBook = async () => {
     if (title.trim() != " " && author.trim() != " ") {
-      const newBook = { title, author };
       try {
-        const docRef = await addDoc(collection(db, "books"), newBook);
-        console.log("Document written with ID: ", docRef.id);
-      } catch (e) {
-        console.error("Error adding document: ", e);
+        const newBook = { title, author };
+        // try {
+        //   const docRef = await addDoc(collection(db, "books"), newBook);
+        //   console.log("Document written with ID: ", docRef.id);
+        // } catch (e) {
+        //   console.error("Error adding document: ", e);
+        // }
+        await axios.post("http://localhost:8000/api/add_readinglist", newBook);
+        setBooks([...books, newBook]);
+        setTitle("");
+        setAuthor("");
+      } catch (error) {
+        console.error("Error adding task:", error);
       }
-      setBooks([...books, { title, author }]);
-      setTitle("");
-      setAuthor("");
     }
   };
 
   const removeBook = async (index: number) => {
-    const bookToRemove = books[index];
-    const booksCollection = collection(db, "books");
-    const querySnapshot = await getDocs(booksCollection);
-    querySnapshot.forEach((doc) => {
-      const bookData = doc.data() as Book;
-      if (
-        bookData.title === bookToRemove.title &&
-        bookData.author === bookToRemove.author
-      ) {
-        deleteDoc(doc.ref);
-      }
-    });
-    const updatedBooks = [...books];
-    updatedBooks.splice(index, 1);
-    setBooks(updatedBooks);
+    // const bookToRemove = books[index];
+    // const booksCollection = collection(db, "books");
+    // const querySnapshot = await getDocs(booksCollection);
+    // querySnapshot.forEach((doc) => {
+    //   const bookData = doc.data() as Book;
+    //   if (
+    //     bookData.title === bookToRemove.title &&
+    //     bookData.author === bookToRemove.author
+    //   ) {
+    //     deleteDoc(doc.ref);
+    //   }
+    // });
+    // const updatedBooks = [...books];
+    // updatedBooks.splice(index, 1);
+    // setBooks(updatedBooks);
+    try {
+      const bookToRemove = books[index];
+      await axios.delete("http://localhost:8000/api/delete_readinglist/");
+      const updatedBooks = [...books];
+      updatedBooks.splice(index, 1);
+      setBooks(updatedBooks);
+    } catch (error) {
+      console.log("error while removing book: ", error);
+    }
   };
 
   return (
