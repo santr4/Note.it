b703d9fc22530d490d0a5fe1d5bb775c27f1f6d0
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2024-04-26 15:45:09 +0530

    [added] : docker file  + entrypoint.sh + env added to note.it-backend

---
+++ note.it-backend/noteapp/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# if [ "$DATABASE" = "test3" ]; then
+#    echo "Waiting for postgres..."
+
+#    while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
+#      sleep 0.1
+#    done
+
+#    echo "PostgreSQL started"
+# fi
+
+# Make migrations and migrate the database.
+
+echo "Making migrations and migrating the database."
+python manage.py makemigrations --noinput 
+python manage.py migrate --noinput 
+python manage.py collectstatic --noinput
+gunicorn noteapp.wsgi:application --bind 0.0.0.0:8000 --workers=2
+
+exec "$@"
--- note.it-backend/noteapp/noteapp/settings.py
+++ note.it-backend/noteapp/noteapp/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -79,13 +78,17 @@ WSGI_APPLICATION = "noteapp.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": os.getenv("DATABASE"),
+    #     "USER": os.getenv("POSTGRES_USERNAME"),
+    #     "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+    #     "HOST": os.getenv("DATABASE_HOST"),
+    #     "PORT": os.getenv("DATABASE_PORT"),
+    # }
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "test3",
-        "USER": "aaranyak",
-        "PASSWORD": "160903belda",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
